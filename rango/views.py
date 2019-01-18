5a7c69fcdee7dcd40ffa5377592d36f299092b51
Mingfeng YE <2325714y@student.gla.ac.uk>
2019-01-18 10:21:18 +0000

    adding a template. 4.1
    Serving Static Media Files  4.2

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+    #construct a dictionary to pass to the template engine as its context
+    # Note the key boldmessage is the same as {{boldmessage}} in the template
+    context_dict = {'boldmessage': "chunchy,creamy,cookie,candy,cupcake!"}
+    return render(request, 'rango/index.html',context = context_dict)
 
 def about(request):
     return HttpResponse("this is the about test! <a href='/rango/'>Index</a>")
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,7 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#print(__file__)
+#print(os.path.dirname(__file__))
+#print(os.path.dirname(os.path.dirname(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -52,10 +54,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'tango_with_django_project.urls'
 
+#the way to aoid hard code path
+#chapter 4 18.1.2019
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR,]
