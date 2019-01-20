cdee392f28419ac3bb90960eafe3260920a0e3bb
Mingfeng YE <2325714y@student.gla.ac.uk>
2019-01-20 12:38:13 +0000

    update category and index html view    6.2

--- populate_rango.py
+++ populate_rango.py
@@ -44,7 +44,7 @@ def populate():
     # if you want to add more categories or pages,
     # add them to the dictionaries above
 
-
+    print(cats["Python"]["views"])
 
     # the code below goes through the cates dictionary, then adds each category,
     # and then adds all the associated pages for that category.
@@ -54,6 +54,7 @@ def populate():
 
     for cat, cat_data in cats.items():
         c = add_cat(cat,cat_data["views"],cat_data["likes"])
+
         for p in cat_data["pages"]:
             add_page(c,p["title"],p["url"])
 
@@ -65,7 +66,9 @@ def populate():
 
 
 def add_cat(name,views,likes):
-    c = Category.objects.get_or_create(name = name,views = views,likes= likes)[0]
+    c = Category.objects.get_or_create(name = name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
