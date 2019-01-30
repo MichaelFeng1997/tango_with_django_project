a11bf1f9a25ac6ee2d933d1a218e5afda6757279
Mingfeng YE <2325714y@student.gla.ac.uk>
2019-01-30 21:47:14 +0000

    chapter 10 and fix some bugs

--- rango/views.py
+++ rango/views.py
@@ -11,7 +11,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-
+from datetime import datetime
 
 def show_category(request,category_name_slug):
     # Create a context dictionary which we can pass
@@ -52,12 +52,30 @@ def index(request):
     # Retrieve the top 5 only - or all if less than 5
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
+
+    request.session.set_test_cookie()
+
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories':category_list,'pages':page_list}
-    return render(request, 'rango/index.html',context = context_dict)
+
+    # call the helper function to handle the cookies
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    #Obtain our Response object early so we can add cookie information
+    response = render(request,'rango/index.html',context = context_dict)
+
+
+
+    return response
 
 def about(request):
+
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
+
     # print out whether the method is a GET or a POST
     print(request.method)
     #print out the user name, if no one is logged in it prints 'AnonymousUser'
@@ -216,6 +234,35 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object
         return render(request,'rango/login.html',{})
+
+
+# A helper method
+def get_server_side_cookie(request,cookie,default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
 
 @login_required
 def restricted(request):
